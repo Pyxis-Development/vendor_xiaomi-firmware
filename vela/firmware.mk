8ba361dde6465905d3d058fba95531068f75a598
TheMalachite <eliasgheeraert@gmail.com>
2020-05-02 21:22:04 +0200

    frimware: Add missing firmware.mk

---
+++ vela/firmware.mk
@@ -0,0 +1,20 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/firmware/vela/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi/firmware/vela/aop.mbn:install/firmware-update/aop.mbn \
+    vendor/xiaomi/firmware/vela/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/xiaomi/firmware/vela/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/xiaomi/firmware/vela/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/xiaomi/firmware/vela/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/xiaomi/firmware/vela/dspso.bin:install/firmware-update/dspso.bin \
+    vendor/xiaomi/firmware/vela/dtbo.img:install/firmware-update/dtbo.img \
+    vendor/xiaomi/firmware/vela/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/xiaomi/firmware/vela/keymaster64.mbn:install/firmware-update/keymaster64.mbn \
+    vendor/xiaomi/firmware/vela/logfs_ufs_8mb.bin:install/firmware-update/logfs_ufs_8mb.bin \
+    vendor/xiaomi/firmware/vela/logo.img:install/firmware-update/logo.img \
+    vendor/xiaomi/firmware/vela/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/xiaomi/firmware/vela/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+    vendor/xiaomi/firmware/vela/storsec.mbn:install/firmware-update/storsec.mbn \
+    vendor/xiaomi/firmware/vela/tz.mbn:install/firmware-update/tz.mbn \
+    vendor/xiaomi/firmware/vela/xbl.elf:install/firmware-update/xbl.elf \
+    vendor/xiaomi/firmware/vela/xbl_config.elf:install/firmware-update/xbl_config.elf
